389d724bd10bd9fb7e24a2e02bc6429772c26d12
Val Kenneth Arado <37638186+Vaaaaaalllll@users.noreply.github.com>
2026-02-26 11:47:17 +0800

    Upload from Dify

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# WARNING: template code, may need edits
+"""Paragraph Checker - A learning-focused grammar and writing analysis tool."""
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+
+from src.checker import ParagraphChecker
+
+__all__ = ["ParagraphChecker"]
